99e262ba3acde8478f3776037ec152bcd2a2675a
Eduardo <eddeav98@gmail.com>
2020-06-17 00:41:36 -0500

    Mensaje en LCD sin Driver

---
+++ MKL25Z128xxx4_LCD_LCD/source/MKL25Z128xxx4_LCD_LCD.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MKL25Z4.h"
+#include "fsl_debug_console.h"
+#include <string.h>
+#include "fsl_tpm.h"
+
+#define DELAY 18000
+
+
+void LCD_Data(uint8_t *high, uint8_t *low, uint8_t c);
+void Write_Data(uint8_t data);
+void LCD_CMD(uint8_t cmd);
+void LCD_Write(uint8_t data);
+void LCD_Clear();
+void LCD_Return();
+void LCD_Mode(uint8_t ID, uint8_t S);
+void LCD_Set(uint8_t D, uint8_t C, uint8_t B);
+void LCD_Cursor(uint8_t SC, uint8_t RL);
+void LCD_Activate(uint8_t DL, uint8_t N, uint8_t F);
+void LCD_CGRAM(uint8_t Address);
+void LCD_DDRAM(uint8_t Address);
+void Delay (uint32_t delay);
+void Line (int i);
+void Drucken_LCD(char Nachricht[]);
+
+int main(void) {
+
+
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+    BOARD_InitDebugConsole();
+
+    LCD_Activate(1u,1u,0u);
+
+    LCD_Set(1u,1u,1u);
+	LCD_Clear();
+	Drucken_LCD("Hallo Welt! Was ist los?");
+
+
+
+    return 0 ;
+}
+
+void LCD_Data(uint8_t *high, uint8_t *low, uint8_t c)
+{
+  *high=(c & 0xF0) >> 4;
+  *low= c & 0x0F;
+}
+void Write_Data(uint8_t data)
+{
+
+	for (uint8_t i=0;i<4;i++) //low
+	{
+		if(data & (1u<<i))
+		{
+			GPIO_SetPinsOutput(GPIOB, 1u<<i);
+		}
+		else
+		{
+			GPIO_ClearPinsOutput(GPIOB, 1u<<i);
+		}
+	}
+
+
+	for (uint8_t i=0;i<4;i++) //High
+	{
+		if(data & (1u<<(i+4)))
+		{
+			GPIO_SetPinsOutput(GPIOE, 1u<<(i+20));
+		}
+		else
+		{
+			GPIO_ClearPinsOutput(GPIOE, 1u<<(i+20));
+		}
+	}
+}
+
+void LCD_CMD(uint8_t cmd)
+{
+	//Pone LCD_RS en bajo
+	GPIO_ClearPinsOutput(GPIOC, 1u<<BOARD_INITPINS_RS_PIN);
+	//Pone LCD_RW en bajo
+	GPIO_ClearPinsOutput(GPIOC, 1u<<BOARD_INITPINS_RW_PIN);
+	//Pone LCD_E en alto
+	GPIO_SetPinsOutput(GPIOE, 1u<<BOARD_INITPINS_E_PIN);
+	Write_Data(cmd);
+	//Pone LCD_E en bajo
+	GPIO_ClearPinsOutput(GPIOE, 1u<<BOARD_INITPINS_E_PIN);
+	Delay(DELAY);
+
+}
+void LCD_Write(uint8_t data)
+{
+	//Pone LCD_RS en alto
+	GPIO_SetPinsOutput(GPIOC, 1u<<BOARD_INITPINS_RS_PIN);
+	//Pone LCD_RW en bajo
+	GPIO_ClearPinsOutput(GPIOC, 1u<<BOARD_INITPINS_RW_PIN);
+	//Pone LCD_E en alto
+	GPIO_SetPinsOutput(GPIOE, 1u<<BOARD_INITPINS_E_PIN);
+	Write_Data(data);
+	//Pone LCD_E en bajo
+	GPIO_ClearPinsOutput(GPIOE, 1u<<BOARD_INITPINS_E_PIN);
+	Delay(DELAY);
+}
+void LCD_Clear()
+{
+	uint8_t cmd=0x01u;
+	LCD_CMD(cmd);
+}
+void LCD_Return()
+{
+	uint8_t cmd=0x02u;
+	LCD_CMD(cmd);
+}
+void LCD_Mode(uint8_t ID, uint8_t S)
+{
+	uint8_t cmd=4u;
+	if(ID)
+		cmd |=2u;
+	if(S)
+		cmd |=1u;
+	LCD_CMD(cmd);
+}
+void LCD_Set(uint8_t D, uint8_t C, uint8_t B)
+{
+	uint8_t cmd=8u;
+	if(D)
+		cmd |= 4u;
+	if(C)
+		cmd |= 2u;
+	if(B)
+		cmd |= 1u;
+	LCD_CMD(cmd);
+}
+void LCD_Cursor(uint8_t SC, uint8_t RL)
+{
+	uint8_t cmd=16u;
+	if(SC)
+		cmd |=8u;
+	if(RL)
+		cmd |=4u;
+	LCD_CMD(cmd);
+}
+void LCD_Activate(uint8_t DL, uint8_t N, uint8_t F)
+{
+	uint8_t cmd=32u;
+	if(DL)
+		cmd |=16u;
+	if(N)
+		cmd |=8u;
+	if(F)
+		cmd |=4u;
+	LCD_CMD(cmd);
+}
+void LCD_CGRAM(uint8_t Address)
+{
+	uint8_t cmd = 0x40u;
+	cmd |= Address & 0x3Fu;
+	LCD_CMD(cmd);
+}
+void LCD_DDRAM(uint8_t Address)
+{
+	uint8_t cmd = 0x80u;
+	cmd |= Address & 0x7Fu;
+	LCD_CMD(cmd);
+}
+void Delay(uint32_t delay)
+{
+	for(uint32_t i=0;i<delay;i++)
+		__asm("NOP");
+}
+
+void Line (int i){
+	if (i==2){
+	    LCD_DDRAM(40u);
+	}else if(i==1){
+		LCD_DDRAM(00u);
+	}
+}
+
+void Drucken_LCD(char Nachricht[]){
+	int i = 0;
+	int Zeichenketten = (strlen(Nachricht)-1);
+	while (i <= Zeichenketten){
+		if(i==16){
+			Line(2);
+			LCD_Write(Nachricht[i]);
+			i++;
+		}else{
+			LCD_Write(Nachricht[i]);
+			i++;
+		}
+
+	}
+}
